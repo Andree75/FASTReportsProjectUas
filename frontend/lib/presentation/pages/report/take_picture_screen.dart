f2aa8c6f3099763a516cab9245de54a13e2716ea
Andree75 <jamanbuntutan75@gmail.com>
2026-01-15 13:51:56 +0700

    add screen a take picture

---
+++ frontend/lib/presentation/pages/report/take_picture_screen.dart
@@ -0,0 +1,95 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({Key? key}) : super(key: key);
+
+  @override
+  _TakePictureScreenState createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController? _controller;
+  Future<void>? _initializeControllerFuture;
+  bool _isCameraReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  Future<void> _initCamera() async {
+    try {
+      final cameras = await availableCameras();
+
+      if (cameras.isEmpty) {
+        throw Exception('Tidak ada kamera ditemukan');
+      }
+
+      final firstCamera = cameras.first;
+
+      _controller = CameraController(firstCamera, ResolutionPreset.medium);
+
+      _initializeControllerFuture = _controller!.initialize();
+      await _initializeControllerFuture;
+
+      if (mounted) {
+        setState(() {
+          _isCameraReady = true;
+        });
+      }
+    } catch (e) {
+      print("Error Camera: $e");
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Gagal membuka kamera: $e")));
+        Navigator.pop(context);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Ambil Foto'),
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              _isCameraReady) {
+            return Center(child: CameraPreview(_controller!));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller!.takePicture();
+            if (mounted) {
+              Navigator.pop(context, image.path);
+            }
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
