123bcfcdfd6350146de3b873e534f52d5621903a
Andree75 <jamanbuntutan75@gmail.com>
2026-01-22 18:02:21 +0700

    update database sql

--- backend/fast_reports_db.sql
+++ backend/fast_reports_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 18, 2026 at 03:57 PM
--- Server version: 10.11.13-MariaDB-0ubuntu0.24.04.1
--- PHP Version: 8.3.6
+-- Waktu pembuatan: 22 Jan 2026 pada 11.00
+-- Versi server: 10.11.13-MariaDB-0ubuntu0.24.04.1
+-- Versi PHP: 8.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reports`
+-- Struktur dari tabel `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`, `full_name`) VALUES
+(1, 'admin', 'admin123', 'Super Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reports`
 --
 
 CREATE TABLE `reports` (
@@ -38,24 +58,27 @@ CREATE TABLE `reports` (
   `latitude` double DEFAULT NULL,
   `longitude` double DEFAULT NULL,
   `urgency` varchar(20) NOT NULL DEFAULT 'Biasa',
-  `created_at` timestamp NULL DEFAULT current_timestamp()
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `status` enum('Pending','Diproses','Ditindak Lanjuti','Selesai','Ditolak','Ditangguhkan') DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `reports`
+-- Dumping data untuk tabel `reports`
 --
 
-INSERT INTO `reports` (`id`, `user_id`, `category`, `title`, `description`, `metadata`, `image_path`, `latitude`, `longitude`, `urgency`, `created_at`) VALUES
-(21, 1, 'Lainnya', 'Bencana Banjir besar', '1. pembalakan hutan liar\n2. curah hujan tinggi penyebab banjir bandang datang', '{\"jenis bahaya\":\"Banjir Bandang\",\"lokasi patokan\":\"Aceh tengah\",\"bantuan diperlukan\":\"Satgas Medis, TNI, dan Dewan Pusat\"}', 'uploads/1767868634_b0c328a0-d1ae-11f0-83cd-5b415fb0080a.jpg', 0, 0, 'Tinggi', '2026-01-08 10:37:14'),
-(22, 1, 'Lainnya', 'pembunuhan misterius', 'dsvawegvwrgvrwe', '{\"jenis bahaya\":\"pembunuhan\",\"lokasi patokan\":\"Kebommas\",\"bantuan diperlukan\":\"Satgas Medis\"}', 'uploads/1767949131_logo-uisi.jpg', 0, 0, 'DARURAT', '2026-01-09 08:58:51'),
-(26, 1, 'Kekerasan Fisik', 'Pemukulan sepihak', 'YTTA', '{\"nama_korban\":\"Udin, Ofin, Arie\",\"jenis luka : \":\"Lebam, Memar sedang\"}', 'uploads/1767983497_linux_cam_1767983435484.jpg', 0, 0, 'Sedang', '2026-01-09 18:31:37'),
-(27, 1, 'Judol', 'Judol Gresik', 'Parah anjay', '{\"link situs : \":\"zeusmax88\",\"rekening bandar : \":\"tidak tahu\",\"platform : \":\"Situs Judi\"}', 'uploads/1768212964_linux_cam_1768212911673.jpg', 0, 0, 'Sedang', '2026-01-12 10:16:04'),
-(28, 1, 'Judol', 'JUDI GRESIK PART 2', 'TOLONG JANGAN TELFON POLISI YA :))))', '{\"link situs : \":\"ZUESMAXWINN88\",\"rekening bandar : \":\"098780998909\",\"platform : \":\"SITUS JUDI HARAM\"}', 'uploads/1768377673_linux_cam_1768377474814.jpg', 0, 0, 'Sedang', '2026-01-14 08:01:13');
+INSERT INTO `reports` (`id`, `user_id`, `category`, `title`, `description`, `metadata`, `image_path`, `latitude`, `longitude`, `urgency`, `created_at`, `status`) VALUES
+(21, 1, 'Lainnya', 'Bencana Banjir besar', '1. pembalakan hutan liar\n2. curah hujan tinggi penyebab banjir bandang datang', '{\"jenis bahaya\":\"Banjir Bandang\",\"lokasi patokan\":\"Aceh tengah\",\"bantuan diperlukan\":\"Satgas Medis, TNI, dan Dewan Pusat\"}', 'uploads/1767868634_b0c328a0-d1ae-11f0-83cd-5b415fb0080a.jpg', 0, 0, 'Tinggi', '2026-01-08 10:37:14', 'Pending'),
+(26, 1, 'Kekerasan Fisik', 'Pemukulan sepihak', 'YTTA', '{\"nama_korban\":\"Udin, Ofin, Arie\",\"jenis luka : \":\"Lebam, Memar sedang\"}', 'uploads/1767983497_linux_cam_1767983435484.jpg', 0, 0, 'Sedang', '2026-01-09 18:31:37', 'Ditindak Lanjuti'),
+(27, 1, 'Judol', 'Judol Gresik', 'Parah anjay', '{\"link situs : \":\"zeusmax88\",\"rekening bandar : \":\"tidak tahu\",\"platform : \":\"Situs Judi\"}', 'uploads/1768212964_linux_cam_1768212911673.jpg', 0, 0, 'Sedang', '2026-01-12 10:16:04', 'Pending'),
+(28, 1, 'Judol', 'JUDI GRESIK PART 2', 'TOLONG JANGAN TELFON POLISI YA :))))', '{\"link situs : \":\"ZUESMAXWINN88\",\"rekening bandar : \":\"098780998909\",\"platform : \":\"SITUS JUDI HARAM\"}', 'uploads/1768377673_linux_cam_1768377474814.jpg', 0, 0, 'Sedang', '2026-01-14 08:01:13', 'Ditolak'),
+(31, 1, 'Pelecehan', 'Pelecehan jalan kebomas', '1.\n2.\n3.', '{\"ciri pelaku : \":\"tinggi 180, bertindi\",\"saksi\":\"tidak\"}', 'uploads/1768965254_L.jpg', 0, 0, 'DARURAT', '2026-01-21 03:14:14', 'Ditangguhkan'),
+(32, 1, 'Narkoba', 'efvDSfvs', 'egfdsfvdsf', '{\"jenis obat : \":\"efefesf\",\"lokasi transaksi : \":\"eafeafeaf\"}', 'uploads/1768965336_linux_cam_1768965330942.jpg', 0, 0, 'Tinggi', '2026-01-21 03:15:36', 'Selesai'),
+(33, 1, 'Judol', 'rfvreav', 'debrebr', '{\"link situs : \":\"wevrvgerv\",\"rekening bandar : \":\"reveraver\",\"platform : \":\"vdevervf\"}', 'uploads/1769008313_linux_cam_1769008302731.jpg', 0, 0, 'Sedang', '2026-01-21 15:11:53', 'Diproses');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -67,52 +90,67 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `full_name`, `username`, `password`, `created_at`) VALUES
-(1, 'andre darmawan', 'andre', '$2y$10$eYjLA9Ixr6qDAg.g2W.LNO9plfCkTRv4gKx4NCEF1aWbHa1Fw7JHS', '2026-01-08 03:26:53');
+(1, 'andre darmawan', 'andre', '$2y$10$eYjLA9Ixr6qDAg.g2W.LNO9plfCkTRv4gKx4NCEF1aWbHa1Fw7JHS', '2026-01-08 03:26:53'),
+(3, 'Steven Watanabe', 'steven1', '$2y$10$/xoYg0csmGUeuVIGg/.7E.XHrMtChfoaR82mfmenpd.S.2sC1RobC', '2026-01-20 10:01:17'),
+(4, 'andri D', 'andriii', '$2y$10$a3wpjYXBoHmllclPWtiNz.QJbz7db0HmAVc4q1SwbFjEbqPOKGGOq', '2026-01-21 03:18:53');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `reports`
+-- Indeks untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `reports`
 --
 ALTER TABLE `reports`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_report` (`user_id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `reports`
+-- AUTO_INCREMENT untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `reports`
+-- Ketidakleluasaan untuk tabel `reports`
 --
 ALTER TABLE `reports`
   ADD CONSTRAINT `fk_user_report` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
